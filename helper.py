3638185a37343f9e4a80de5a8ae7eb98bc474948
KennFatt <clashofklen@gmail.com>
2020-03-12 17:32:28 +0700

    Helper: Auto generate source footer

--- helper.py
+++ helper.py
@@ -2,6 +2,20 @@
 
 import os
 import sys
+import time
+
+
+def get_timestamp():
+    return time.asctime(time.localtime(time.time()))
+
+
+def get_kernel_release():
+    return os.popen("uname -r").read().strip("\n")
+
+
+def get_gcc_version():
+    parse = os.popen("gcc --version").read()
+    return parse.split("\n")[0]
 
 
 def new_sample(sample_name):
@@ -12,6 +26,22 @@ def new_sample(sample_name):
     sample_path = os.path.realpath(sample_name)
     os.makedirs(os.path.join(sample_path, "out"), 0o777)
     print(f"New directory has been created at: {sample_path}")
+
+    with open(os.path.join(sample_path, "main.cpp"), "w") as f:
+        footer = {
+            "Created on: ": get_timestamp(),
+            "Compiler: ": get_gcc_version(),
+            "OS: ": get_kernel_release(),
+            "Flags: ": "-Wall -std=c++11 -g"
+        }
+
+        # Print the footer
+        f.write("/**\n")
+        for k in footer:
+            f.write(f" * {k}{footer[k]}\n")
+        f.write(" */\n")
+
+        f.close()
 
 
 if __name__ == "__main__":
